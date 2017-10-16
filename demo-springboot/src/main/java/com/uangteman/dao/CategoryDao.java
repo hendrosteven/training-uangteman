2126d84c6587c83fb80b94532d73e03ea77b1d25
Hendro Steven <hendro.steven@gmail.com>
2017-10-16 13:26:14 +0800

    add dao class

---
+++ demo-springboot/src/main/java/com/uangteman/dao/CategoryDao.java
@@ -0,0 +1,57 @@
+package com.uangteman.dao;
+
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.jdbc.core.JdbcTemplate;
+import org.springframework.jdbc.core.RowMapper;
+
+import com.uangteman.model.Category;
+
+public class CategoryDao {
+
+	@Autowired
+	private JdbcTemplate template;
+
+	public int insert(Category category) {
+		return this.template.update("insert into tcategory(name) values(?)", category.getName());
+	}
+
+	public int update(Category category) {
+		return this.template.update("update tcategory set name=? where " + "id=?",
+				category.getName(),
+				category.getId());
+	}
+
+	public List<Category> findAll() {
+		return this.template.query("select id,name from tcategory", 
+				new RowMapper<Category>() {
+			@Override
+			public Category mapRow(ResultSet rs, int index) 
+					throws SQLException {
+				Category category = new Category();
+				category.setId(rs.getInt("id"));
+				category.setName(rs.getString("name"));
+				return category;
+			}
+
+		});
+	}
+	
+	public Category findById(int id){
+		return this.template.queryForObject("select id,name from tcategory "
+				+ "where id=?", new Object[]{id}, new RowMapper<Category>() {
+					@Override
+					public Category mapRow(ResultSet rs, int index) 
+							throws SQLException {
+						Category category = new Category();
+						category.setId(rs.getInt("id"));
+						category.setName(rs.getString("name"));
+						return category;
+					}
+
+				});
+	}
+}
